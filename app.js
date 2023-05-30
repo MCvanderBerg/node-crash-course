8237460c01da24b6207b42052ea653d9d2b631d8
Christiaan Van Der Berg <christiaanvanderberg@Mary-Nel.local>
2023-05-30 22:54:23 +0200

    added some routes to app.js, completed style.css, filled in 404.ejs and other

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const express = require('express');
+const Blog = require('./models/blog')
 
-const app = express()
+const app = express();
 mongoose.connect('mongodb+srv://ChristiaanVanDerBerg:Noeline101@mycluster.nhdujgl.mongodb.net/')
     .then(() => {
         app.listen(3000)
@@ -12,11 +13,20 @@ mongoose.connect('mongodb+srv://ChristiaanVanDerBerg:Noeline101@mycluster.nhdujg
         console.log('Could not connect to the database')
     })
 
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>{
     res.redirect('/blogs')
 })
 
 app.get('/blogs', (req, res)=> {
-    res.render('blogs/index');
+    Blog.find()
+        .then((result) => {
+            res.render('blogs/index', { title: 'Blogs', blogs: result });
+        })
+})
+
+app.use((req, res) => {
+    res.status(404)
+       .render('404', { title: '404' })
 })
